f6ea450d4d3b59dac0407b73aec812e706d0d7cf
Izzatulloh Eshimboyev <eshimboevizzatullos@gmail.com>
2026-03-30 20:24:16 +0500

    site created. JavaScript added. works finished

---
+++ main.js
@@ -0,0 +1,28 @@
+let familiyasi = "Eshimboyev";
+const nechanchi_yil = "2009";
+let imtihondan_otdimi = true;
+let havo_harorati = "19°C";
+const okeanNomi = "Tinch okeani";
+
+let ismi = "Izzatulloh";
+let yosh = 16;
+let shahar = "Farg'ona";
+const it_akademiyasi = "Najot Ta'lim";
+let dasturchimanmi = true;
+
+console.log(familiyasi);
+console.log(nechanchi_yil);
+console.log(imtihondan_otdimi);
+console.log(havo_harorati);
+console.log(okeanNomi);
+
+console.log(ismi);
+console.log(yosh);
+console.log(shahar);
+console.log(it_akademiyasi);
+console.log(dasturchimanmi);
+
+const narx = 500;
+let ism = "Ali";
+let familiya = "Valiyev";
+console.log(familiya);
